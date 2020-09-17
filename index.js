74c527636075ab118bb11538584dd35c1ceab6c3
yoneda <nsshowdream@gmail.com>
2020-09-18 02:14:45 +0900

    [fix] add some components

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import request from "superagent";
 
-function HelloMessage(props) {
-  const { name } = props;
-  return <div>hello, {name}</div>;
+function Home(props) {
+  return <div>This is home page</div>;
 }
 
-ReactDOM.render(<HelloMessage name="Jane" />, document.getElementById("app"));
+function About(props) {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const url = "https://reqres.in/api/users/1";
+    request
+      .get(url)
+      .then((res) => res.body)
+      .then((body) => {
+        setName(body.data.first_name);
+      });
+  }, []);
+  return (
+    <div>
+      <div>This is about page</div>
+      {name && <div>{name}</div>}
+    </div>
+  );
+}
+
+function Works(props) {
+  return (
+    <div>
+      <div>This is works page</div>
+      <p>Please check my awesome works.</p>
+    </div>
+  );
+}
+
+function Contact(props) {
+  return (
+    <div>
+      <div>This is contact page</div>
+      <p>Mail to me.</p>
+    </div>
+  );
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="about">about</Link> |{" "}
+      <Link to="works">works</Link> | <Link to="contact">contant</Link>
+    </nav>
+  );
+}
+
+function App(props) {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const url = "https://reqres.in/api/users/1";
+    request
+      .get(url)
+      .then((res) => res.body)
+      .then((body) => {
+        setName(body.data.first_name);
+      });
+  }, []);
+  return (
+    <div>
+      <Nav />
+      <Router>
+        <Home path="/" />
+        <About path="/about" />
+        <Works path="/works" />
+        <Contact path="/contact" />
+      </Router>
+      {name && <div>name is {name}</div>}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
